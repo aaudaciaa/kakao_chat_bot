5a05fc36c7a14823ebc17feb0e80159b8dfd7399
aaudaciaa <aaudaciaa@gmail.com>
2017-12-29 06:29:49 +0000

    add Module

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -1,3 +1,5 @@
+require 'parser'
+
 class KakaoController < ApplicationController
   def keyboard
     home_keyboard = {
@@ -25,38 +27,31 @@ class KakaoController < ApplicationController
     elsif user_message == "메뉴"
       return_text = ["치킨", "피자", "햄버거", "라면", "짜장면", "20층"].sample(1).to_s
 
+    # 고양이 사진 보여주기
     elsif user_message == "고양이"
-      # 고양이 사진 보여주기
       image = true
-      url = "http://thecatapi.com/api/images/get?format=xml&type=jpg"
-      cat_xml = RestClient.get(url)
-      doc = Nokogiri::XML(cat_xml)
-      cat_url = doc.xpath("//url").text
+      cat_photo = Parser::Animal.new
+      cat_info = cat_photo.cat
+
+      return_text = cat_info[0]
+      img_url = cat_info[1]      
+
 
     # 영화 보여주기
-  elsif user_message == "영화"
-  		image = true
-  		url = "http://movie.naver.com/movie/running/current.nhn?view=list&tab=normal&order=reserve"
-  		movie_html = RestClient.get(url)
-  		doc = Nokogiri::HTML(movie_html)
+    elsif user_message == "영화"
+      image = true
+      naver_movie = Parser::Movie.new
+      naver_movie_info = naver_movie.naver
 
-  		movie_title = Array.new
-  		movie_info = Hash.new
+      return_text = naver_movie_info[0]
+      img_url = naver_movie_info[1]
 
-  		doc.css("ul.lst_detail_t1 dt a").each do |title|
-  			movie_title << title.text
-  		end
+    elsif user_message == "코인"
+      url = "https://www.coinnest.co.kr/market-tron"
+      doc = Nokogiri::HTML(open(url))
 
-		  doc.css("ul.lst_detail_t1 li").each do |movie|
-  			movie_info[movie.css("dl dt.tit a").text] = {
-  				url: movie.css("div.thumb img").attribute('src').to_s,
-  				star: movie.css("dl.info_star span.num").text
-  			}
-  		end
+      return_text = doc.css("dl.all_coin_info dt").text
 
-  		sample_movie = movie_title.sample
-  		return_text = sample_movie + " " + movie_info[sample_movie][:star]
-  		cat_url = movie_info[sample_movie][:url]
 
     # 다른 명령어가 들어왔을 때 => ㅠㅠ 알 수 없는 명령어 입니다 출력되게
     else
@@ -73,7 +68,7 @@ class KakaoController < ApplicationController
       message: {
         text: return_text,
         photo: {
-          url: cat_url,
+          url: img_url,
           width: 640,
           height: 480
         }
---
+++ app/helpers/parser.rb
@@ -0,0 +1,41 @@
+module Parser
+  class Movie
+    def naver
+  		url = "http://movie.naver.com/movie/running/current.nhn?view=list&tab=normal&order=reserve"
+  		movie_html = RestClient.get(url)
+  		doc = Nokogiri::HTML(movie_html)
+
+  		movie_title = Array.new
+  		movie_info = Hash.new
+
+  		doc.css("ul.lst_detail_t1 dt a").each do |title|
+  			movie_title << title.text
+  		end
+
+		  doc.css("ul.lst_detail_t1 li").each do |movie|
+  			movie_info[movie.css("dl dt.tit a").text] = {
+  				url: movie.css("div.thumb img").attribute('src').to_s,
+  				star: movie.css("dl.info_star span.num").text
+  			}
+  		end
+
+  		sample_movie = movie_title.sample
+  		return_text = sample_movie + " " + movie_info[sample_movie][:star]
+  		cat_url = movie_info[sample_movie][:url]
+
+      return [return_text, cat_url]
+    end
+  end
+
+  class Animal
+    def cat
+      url = "http://thecatapi.com/api/images/get?format=xml&type=jpg"
+      cat_xml = RestClient.get(url)
+      doc = Nokogiri::XML(cat_xml)
+      cat_url = doc.xpath("//url").text
+      return_text = "고양이 사진이요ㅎㅎ"
+
+      return [return_text, cat_url]
+    end
+  end
+end
